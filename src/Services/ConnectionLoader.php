435b761ee966e245416f7872da67ac7b2b1ae609
Nahom <nahomt@amestsantim.com>
2023-04-27 08:11:16 +0300

    Added multiple database driver support for sources

--- src/Services/ConnectionLoader.php
+++ src/Services/ConnectionLoader.php
@@ -16,31 +16,16 @@ class ConnectionLoader
             $connections = collect([]);
         }
         $keyedConnections = $connections->mapWithKeys(function ($item) {
-            return [
-                $item['name'] => [
-                    'driver' => 'mysql',
-                    'url' => null,
-                    'host' => $item['host'],
-                    'port' => $item['port'],
-                    'database' => $item['database'],
-                    'username' => $item['username'],
-                    'password' => $item['password'],
-                    'unix_socket' => env('DB_SOCKET', ''),
-                    'charset' => 'utf8mb4',
-                    'collation' => 'utf8mb4_unicode_ci',
-                    'prefix' => '',
-                    'prefix_indexes' => true,
-                    'strict' => true,
-                    'engine' => null,
-                    'modes' => [
-                        'NO_ENGINE_SUBSTITUTION'
-                    ],
-                    'options' => extension_loaded('pdo_mysql') ? array_filter([
-                        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-                        PDO::ATTR_PERSISTENT => true,
-                    ]) : [],
-                ]
+            $defaultConfig = config('database.connections')[$item['driver']] ?? [];
+            $config = [
+                ...$defaultConfig,
+                'host' => $item['host'],
+                'port' => $item['port'],
+                'database' => $item['database'],
+                'username' => $item['username'],
+                'password' => $item['password'],
             ];
+            return [$item['name'] => $config];
         });
         foreach ($keyedConnections as $name => $connection) {
             config(["database.connections.$name" => $connection]);
