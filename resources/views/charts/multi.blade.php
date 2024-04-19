84750b163568358ddcafaf468e7bb0bed81cd6fc
Nahom <nahomt@amestsantim.com>
2024-04-19 16:08:03 +0300

    Lingering questionnaire to data-source rename

--- resources/views/charts/multi.blade.php
+++ resources/views/charts/multi.blade.php
@@ -8,14 +8,14 @@
 
     <div class="grid grid-cols-1 gap-6 lg:grid-cols-2 sm:p-6 sm:pt-0 pb-6 bg-gray-100 grid-flow-row">
         @forelse($indicators as $indicator)
-            @connectible($indicator->questionnaire)
+            @connectible($indicator->data_source)
                 <x-chimera-chart-card :indicator="$indicator">
                     @livewire($indicator->component, ['indicator' => $indicator])
                 </x-chimera-chart-card>
             @else
                 <x-chimera-simple-card>
                     This indicator is not available because the database connection of the questionnaire
-                    called <b>{{$indicator->questionnaire}}</b> is not available.
+                    called <b>{{$indicator->data_source}}</b> is not available.
                 </x-chimera-simple-card>
             @endconnectible
         @empty
--- resources/views/charts/single.blade.php
+++ resources/views/charts/single.blade.php
@@ -7,14 +7,14 @@
     <livewire:area-filter />
 
     <div class="grid grid-cols-1 gap-6 sm:p-6 sm:pt-0 pb-6 sm:pb-0 bg-gray-100">
-        @connectible($indicator->questionnaire)
+        @connectible($indicator->data_source)
             <x-chimera-chart-card :indicator="$indicator" mode="Full Page">
                 @livewire($indicator->component, ['indicator' => $indicator])
             </x-chimera-chart-card>
         @else
             <x-chimera-simple-card>
                 This indicator is not available because the database connection of the questionnaire
-                called <b>{{ $indicator->questionnaire }}</b> is not available.
+                called <b>{{ $indicator->data_source }}</b> is not available.
             </x-chimera-simple-card>
         @endconnectible
     </div>
