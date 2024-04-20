08de31f1203be3baf995edf24a172c386c60e86c
Nahom <nahomt@amestsantim.com>
2024-04-20 15:36:23 +0300

    Added 'Run now' function to report management (to generate the report file now!)
    (cherry picked from commit 00c5a76a6971bb004efe7c1539c9f82f59276b5d)

--- resources/views/report/manage/index.blade.php
+++ resources/views/report/manage/index.blade.php
@@ -97,7 +97,8 @@
                                     @if($record->run_every) then every {{ $record->run_every }} hours @endif
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                    <a href="{{route('manage.report.edit', $record->id)}}" class="text-indigo-600 hover:text-indigo-900">{{ __('Edit') }}</a>
+                                    <a href="{{route('manage.report.edit', $record->id)}}" class="text-indigo-600 hover:text-indigo-900">{{ __('Edit') }}</a> |
+                                    <a href="{{route('manage.report.run_now', $record->id)}}" class="text-red-600 hover:text-red-900">{{ __('Run now') }}</a>
                                 </td>
                             </tr>
                         @empty
