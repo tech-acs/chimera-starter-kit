43c6f3f640fd7adcabfae046b56914ef70ed4fe3
Nahom <nahomt@amestsantim.com>
2024-04-30 17:58:33 +0300

    Set locale to http header (HTTP_ACCEPT_LANGUAGE) user preferred

--- src/Http/Middleware/Language.php
+++ src/Http/Middleware/Language.php
@@ -10,9 +10,16 @@ class Language
 {
     public function handle($request, Closure $next)
     {
-        $lang = Cookie::get('locale', 'en');
+        $header = $request->server->get('HTTP_ACCEPT_LANGUAGE');
+        $userPreferredLocale = collect(explode(',', $header))
+            ->map(fn ($locale) => str($locale)->replace('-', '_'))
+            ->first();
+        $lang = Cookie::get('locale', (string)$userPreferredLocale);
         if (array_key_exists($lang, config('languages'))) {
             App::setLocale($lang);
+            Cookie::queue('locale', $lang);
+        } else {
+            App::setLocale('en_US');
         }
         return $next($request);
     }
