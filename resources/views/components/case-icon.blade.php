56558bbf7fa209dd6b7b0381bea3da48aa83a8df
Nahom <nahomt@amestsantim.com>
2024-05-30 21:31:47 +0300

    Case stats color representation swap

--- resources/views/components/case-icon.blade.php
+++ resources/views/components/case-icon.blade.php
@@ -1,9 +1,9 @@
 @if($type === 'total')
-    <div {{ $attributes->merge(['class' => 'bg-green-500 rounded-md p-3']) }}>
+    <div {{ $attributes->merge(['class' => 'bg-blue-500 rounded-md p-3']) }}>
         <svg class="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path></svg>
     </div>
 @elseif($type === 'complete')
-    <div {{ $attributes->merge(['class' => 'bg-blue-500 rounded-md p-3']) }}>
+    <div {{ $attributes->merge(['class' => 'bg-green-500 rounded-md p-3']) }}>
         <svg class="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path><path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm9.707 5.707a1 1 0 00-1.414-1.414L9 12.586l-1.293-1.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
     </div>
 @elseif($type === 'partial')
