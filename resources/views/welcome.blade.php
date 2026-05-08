b867e27ebdd56dae0f6ae999f7759e850d9074dd
Nahom <nahomt@amestsantim.com>
2026-05-08 17:12:22 +0300

    New (branded) landing page look with improved text.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,26 +40,46 @@
         </div>
 
         <main class="lg:relative flex-grow">
-            <div class="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
-                <div class="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
+            <div class="mx-auto max-w-[90%] w-full pb-20 text-center lg:py-24 lg:text-left">
+                <div class="px-4 lg:w-2/5 sm:px-8 xl:pr-8">
+
+                    <span class="mb-4 inline-flex items-center gap-x-1.5 rounded-full bg-indigo-100 px-3 py-2 text-sm font-medium text-indigo-700 dark:bg-indigo-400/10 dark:text-indigo-400">
+  <svg viewBox="0 0 6 6" aria-hidden="true" class="size-1.5 fill-indigo-500 dark:fill-indigo-400">
+    <circle r="3" cx="3" cy="3" />
+  </svg>
+  {{ __('Built with Laravel for performance & scale') }}
+</span>
                     <h1 class="tracking-tight font-extrabold text-gray-900 text-4xl sm:text-5xl lg:text-6xl xl:text-7xl">
-                        <span class="block xl:inline">{{ __('The power to manage') }}</span>
-                        <span class="block text-indigo-600 xl:inline">{{ __('your digital census & survey') }}</span>
+                        <span class="block xl:inline">{{ __('Smarter Census.') }}</span>
+                        <span class="block xl:inline">{{ __('Better Decisions.') }}</span>
+                        <span class="block text-indigo-600 xl:inline">{{ __('Stronger Impact.') }}</span>
                     </h1>
                     <p class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
                     {{ __('description of dashboard on landing page') }}
                     </p>
                     <div class="mt-10 sm:flex sm:justify-center lg:justify-start">
                         <div class="rounded-md shadow">
-                            <a href="{{ url('/home') }}" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
-                                {{ __('Get started') }}
+                            <a href="{{ url('/home') }}" class="w-full flex items-center gap-x-1.5 justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="-ml-0.5 mr-2 -mt-1 size-5">
+                                    <path fill-rule="evenodd" d="M9.315 7.584C12.195 3.883 16.695 1.5 21.75 1.5a.75.75 0 0 1 .75.75c0 5.056-2.383 9.555-6.084 12.436A6.75 6.75 0 0 1 9.75 22.5a.75.75 0 0 1-.75-.75v-4.131A15.838 15.838 0 0 1 6.382 15H2.25a.75.75 0 0 1-.75-.75 6.75 6.75 0 0 1 7.815-6.666ZM15 6.75a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Z" clip-rule="evenodd" />
+                                    <path d="M5.26 17.242a.75.75 0 1 0-.897-1.203 5.243 5.243 0 0 0-2.05 5.022.75.75 0 0 0 .625.627 5.243 5.243 0 0 0 5.022-2.051.75.75 0 1 0-1.202-.897 3.744 3.744 0 0 1-3.008 1.51c0-1.23.592-2.323 1.51-3.008Z" />
+                                </svg>
+                                {{ __('Get Started') }}
                             </a>
                         </div>
                     </div>
+
+                    <span class="text-sm mt-6 inline-flex items-center gap-x-1.5 text-gray-700">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-indigo-700">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
+</svg>
+
+                        {{ __('Field proven in multiple African censuses') }}
+                    </span>
                 </div>
             </div>
-            <div class="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">
-                <img class="absolute inset-0 w-full h-full object-center object-cover min-w-96" src="{{ asset('images/hero.jpg', config('chimera.secure')) }}" alt="">
+            <div class="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-3/5 lg:h-full">
+                <img class="absolute inset-0 w-full h-full object-center object-cover min-w-96" src="{{ asset('images/hero.png', config('chimera.secure')) }}" alt="">
             </div>
         </main>
 
