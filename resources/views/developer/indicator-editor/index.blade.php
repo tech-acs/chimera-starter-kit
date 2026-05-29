a28907044072e217b389cdb550e3600ca767bcaf
Nahom <nahomt@amestsantim.com>
2026-05-29 11:46:23 +0300

    Added header bar for indicator title and additional buttons

--- resources/views/developer/indicator-editor/index.blade.php
+++ resources/views/developer/indicator-editor/index.blade.php
@@ -21,7 +21,22 @@
     @endpushonce
 @endif
 <x-app-layout>
-    <div class="h-[calc(100vh-4rem)] flex flex-col">
+    <div class="h-[calc(100vh-12.5rem)] flex flex-col">
+
+        <div class="bg-white border-b border-gray-200 shadow-sm px-4 py-3 flex items-center justify-between shrink-0">
+            <div class="text-lg font-medium text-gray-900">
+                {{ $indicator->title }}
+            </div>
+            <div class="flex items-center gap-2">
+                <x-secondary-button type="button">
+                    {{ __('Undo Changes') }}
+                </x-secondary-button>
+                <x-button type="button">
+                    {{ __('Save as Template') }}
+                </x-button>
+            </div>
+        </div>
+
         <livewire:plotly-editor
             :data-sources="$dataSources"
             :data="$data"
